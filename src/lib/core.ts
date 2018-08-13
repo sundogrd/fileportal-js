f7a423529b39d4c7cff802e440de0d0659fc89fd
MinghaoLu <568432983@qq.com>
2018-08-13 12:29:16 +0800

    rough thinking

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -13,6 +13,10 @@ export interface FilePortalOptions {
 
 export class FilePortal {
   debug;
+  tasks: Task[]
+  taskId: number = 0  // taskId automately increases
+  count: number = 0   // tasks' total amount
+
   constructor(options?: FilePortalOptions) {
     this.debug = new Logger('filePortal/core');
   }
@@ -22,4 +26,85 @@ export class FilePortal {
     /* istanbul ignore next */
     return upload();
   }
+
+  private generateNewTask() {
+    let task = new Task(this.taskId)
+    this.taskId++
+    this.addTask(task)
+  }
+
+  private taskCanceledOrComplete(taskId: number) {
+    for(let i in this.tasks) {
+      if(this.tasks[i].taskId == taskId) {
+        this.tasks.splice(+i, 1)
+        this.count--
+      }
+    }
+  }
+
+  /**
+   * 
+   * @param task represent a process of a file to be uploaded
+   */
+  private addTask(task: Task) {
+    this.tasks.push(task)
+    this.count++
+  }
+
+  /**
+   * 
+   * @param eventName 事件名称
+   * @param toDo      事件处理方法
+   */
+  on(eventName: string, toDo: Function) {
+    this.tasks.forEach(function(task) {
+      task.on(eventName, toDo)
+    }) 
+
+    return this
+  }
+
+  /**
+   * 
+   * @param id a task's id
+   */
+  getTask(id: number): Task {
+    for(var task of this.tasks) {
+      if (task.taskId == id) {
+        return task
+      }
+    }
+
+    return null
+  }
+
+  private notifyAllTasks(eventName: string): void {
+    
+  }
+
+  private notifyOneTask(taskId: number,eventName: string): void {
+
+  }
+
+  // trigger
+
+}
+
+// one file one Task
+class Task {
+  event: Function[]
+  taskId: number
+
+  constructor(taskId: number) {
+    this.taskId = taskId
+  }
+
+  // addEventListener
+  on(eventName: string, toDo: Function) {
+    this.event[eventName] = toDo
+
+    return this
+  }
+
+  // event
 }
