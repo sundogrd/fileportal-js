458c424f56ad3ec729a6238d5866bf4daf2635c6
lwyj123 <liang.pearce@gmail.com>
2018-09-03 20:12:22 +0800

    feat: add task support

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -2,6 +2,8 @@ import { upload } from './api/upload';
 import TaskManager from './TaskManager';
 import { EMimeType } from '../types/upload';
 import Logger from './logger';
+import { ChunkTask } from './task/ChunkTask';
+import DirectTask from './task/DirectTask';
 
 export interface FilePortalOptions {
   debug: boolean;
@@ -12,16 +14,32 @@ export interface FilePortalOptions {
   tokenFun: () => Promise<string>;
 }
 
+export interface AddOptions {
+  immediate: boolean;  // 加入后立即上传
+}
+
 export class FilePortal extends TaskManager {
   debug;
   options: FilePortalOptions;
+  taskManager: TaskManager;
   constructor(options?: FilePortalOptions) {
     super();
     this.debug = new Logger('filePortal/core');
     this.options = options;
+    this.taskManager = new TaskManager();
   }
 
-  upload(file: any) {
+  static BLOCK_SIZE = 4 * 1024 * 1024;
+
+  private generateTask = (file: File) => {
+    if (file.size > FilePortal.BLOCK_SIZE) {
+      return new ChunkTask(file, FilePortal.BLOCK_SIZE, 2 * 1024 * 1024);
+    } else {
+      return new DirectTask(file);
+    }
+  }
+
+  add(file: any, options: AddOptions) {
     this.debug.log('upload start');
     /* istanbul ignore next */
     return upload();
@@ -32,5 +50,4 @@ export class FilePortal extends TaskManager {
       ...options,
     };
   }
-
 }
---
+++ src/lib/task/BaseTask.ts
@@ -0,0 +1,111 @@
+/**
+ * 上传任务
+ */
+abstract class BaseTask {
+  protected _file: File;
+  protected _retry: number = 0;// 已重试次数
+  protected _createDate: Date;// 创建时间
+  protected _startDate: Date;// 开始时间
+  protected _endDate: Date;// 结束时间
+  protected _key: string;// key文件名
+  protected _progress: number = 0;// 任务进度,最大100
+  protected _isSuccess: boolean = false;// 是否上传成功
+  protected _isFinish: boolean = false;// 是否结束
+  protected _result: Object;
+  protected _error: any;
+
+  constructor(file: File) {
+    this._file = file;
+    this._createDate = new Date();
+  }
+
+  public get file(): File {
+    return this._file;
+  }
+
+  public set file(file: File) {
+    this._file = file;
+  }
+
+  get retry(): number {
+    return this._retry;
+  }
+
+  set retry(value: number) {
+    this._retry = value;
+  }
+
+  get createDate(): Date {
+    return this._createDate;
+  }
+
+  set createDate(value: Date) {
+    this._createDate = value;
+  }
+
+  get startDate(): Date {
+    return this._startDate;
+  }
+
+  set startDate(value: Date) {
+    this._startDate = value;
+  }
+
+  get endDate(): Date {
+    return this._endDate;
+  }
+
+  set endDate(value: Date) {
+    this._endDate = value;
+  }
+
+  get isSuccess(): boolean {
+    return this._isSuccess;
+  }
+
+  set isSuccess(value: boolean) {
+    this._isSuccess = value;
+  }
+
+  get progress(): number {
+    return this._progress;
+  }
+
+  set progress(value: number) {
+    this._progress = Math.min(Math.max(0, value), 100);
+  }
+
+  get result() {
+    return this._result;
+  }
+
+  set result(value) {
+    this._result = value;
+  }
+
+  get error() {
+    return this._error;
+  }
+
+  set error(value) {
+    this._error = value;
+  }
+
+  get key(): string {
+    return this._key;
+  }
+
+  set key(value: string) {
+    this._key = value;
+  }
+
+  get isFinish(): boolean {
+    return this._isFinish;
+  }
+
+  set isFinish(value: boolean) {
+    this._isFinish = value;
+  }
+}
+
+export default BaseTask;
