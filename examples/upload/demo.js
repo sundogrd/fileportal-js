f4bcd539171a43183024e66ec946562a15ce0950
lwyj123 <liang.pearce@gmail.com>
2019-06-14 13:43:37 +0800

    feat: using eslint

--- examples/upload/demo.js
+++ examples/upload/demo.js
@@ -1,35 +1,35 @@
 window.addEventListener('DOMContentLoaded', function () {
 
 
-  const onProgress = (evt) => {
-    document.getElementById('progress').innerHTML = `${evt.totalPercent}%`;
-  };
+    const onProgress = (evt) => {
+        document.getElementById('progress').innerHTML = `${evt.totalPercent}%`;
+    };
 
-  document.querySelector('input').addEventListener('change', (event) => {
-    const files = event.target.files;
-    const file = files.item(0);
-    const token = {};
+    document.querySelector('input').addEventListener('change', (event) => {
+        const files = event.target.files;
+        const file = files.item(0);
+        const token = {};
 
-    var reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onload = function () {
-      var uploadFormData = new FormData();
-      var blob = new Blob([reader.result]);
-      uploadFormData.append("ext","png");
-      uploadFormData.append("buffer", blob);
-      debugger
+        var reader = new FileReader();
+        reader.readAsArrayBuffer(file);
+        reader.onload = function () {
+            var uploadFormData = new FormData();
+            var blob = new Blob([reader.result]);
+            uploadFormData.append("ext","png");
+            uploadFormData.append("buffer", blob);
+            debugger
       
-      $.ajax({
-        type: "POST",
-        url: "http://files.lwio.me/multipart/upload",
-        data: uploadFormData,
-        processData: false,
-        contentType: false,
-        success(r) {
-          debugger
-          alert("Success!");
+            $.ajax({
+                type: "POST",
+                url: "http://files.lwio.me/multipart/upload",
+                data: uploadFormData,
+                processData: false,
+                contentType: false,
+                success(r) {
+                    debugger
+                    alert("Success!");
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
